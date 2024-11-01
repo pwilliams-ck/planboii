06153f28ce330856346018404f07910f0e9974fb
pwilliams-ck <pwilliams@cloudkey.io>
2024-11-01 18:51:39 -0500

    hono-01: add hono package, tanstack react query, and hono zod-validator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { cn } from "@/lib/utils";
+import { QueryProvider } from "@/components/query-provider";
 
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -27,7 +28,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <QueryProvider>{children}</QueryProvider>
         </ThemeProvider>
       </body>
     </html>
---
+++ src/features/auth/server/route.ts
@@ -0,0 +1,7 @@
+import { Hono } from "hono";
+
+const app = new Hono().post("/login", (c) => {
+  return c.json({ success: "ok" });
+});
+
+export default app;
